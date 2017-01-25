42db6ccc6ba0978b66becfe0c847f770befe333e
Jacob Keen <jaclkeen@gmail.com>
2017-01-25 13:38:44 -0600

    added habitats. finished zoolandia

--- zoolandia/main.py
+++ zoolandia/main.py
@@ -1,12 +1,26 @@
 from birds.cardinal import Cardinal
 from fish.salmon import Salmon
 from tigers.siberian import Siberian
-
-newSiberian = Siberian()
-print newSiberian.name
-
-newSalmon = Salmon()
-print newSalmon.name
+from habitats import aquarium, savannah
 
 newCardinal = Cardinal()
-print newCardinal.name
+
+newSalmon = Salmon()
+newRedSalmon = Salmon()
+newRedSalmon.name = "Red Salmon"
+newAquarium = aquarium.Aquarium(newSalmon)
+newAquarium.addAnimal(newRedSalmon)
+
+newSiberian = Siberian()
+newBengal = Siberian()
+newBengal.name = "Fred"
+newSavannah = savannah.Savannah(newBengal)
+newSavannah.addAnimal(newSiberian)
+
+print "Savannah"
+for animal in newSavannah.getAnimals():
+    print "    " + animal.name
+
+print "Aquarium: "
+for animal in newAquarium.getAnimals():
+    print "    " + animal.name
